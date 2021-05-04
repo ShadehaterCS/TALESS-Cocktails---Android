fd17f3b6e93f9f32b3be98e85841256be20fdc42
Papavasileiou <antoniopi@csd.auth.gr>
2021-05-04 23:35:12 +0300

    New Activities activity_main=Recipe Overview activity_step= Step of recipe activity_step_with_timer= Step of recipe with integrated timer function

--- app/src/main/java/com/authandroid_smartcookies/smartcookie/StepWithTimer.java
+++ app/src/main/java/com/authandroid_smartcookies/smartcookie/StepWithTimer.java
@@ -3,15 +3,12 @@ package com.authandroid_smartcookies.smartcookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class StepWithTimer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_step_with_timer);
     }
 }
