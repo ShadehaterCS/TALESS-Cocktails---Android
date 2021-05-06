f12f6dc857d8779c989eb25fc105de2b99ed7195
Papavasileiou <antoniopi@csd.auth.gr>
2021-05-06 22:43:53 +0300

    activity_home= στο κατω μερος είναι το nav-bar,στο πανω μερος βρίσκονται τα frangments δλδ οι σελιδες που θα αλλαζουν με βαση το τι πατας στο nav bar
    Το σημαντικο είναι οτι στο HomeActivity.java περιεχετε ενα κωδικας ο οποίος είναι σε comment. Αυτος ο κώδικας παρακολουθεί το που βρισκεται η nav bar και δειχνει την αντίστοιχη σελίδα(fragment) αλλα δυστηχως όταν τρέχει η εφαρμογη στο emulator μου κρασαρει η εφαρμογη,κοιταξτε το μήπως μπορεσετε να το λυσετε πριν απο εμένα
    
    Το splash screen το αφαιρεσα γιατι είχε γίνει μπαχαλο με τα fragments κτλ αλλα θα γίνει

---
+++ app/src/main/java/com/authandroid_smartcookies/smartcookie/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.authandroid_smartcookies.smartcookie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+
+       /*BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);*/
+
+
+    }
+
+
+}
+
