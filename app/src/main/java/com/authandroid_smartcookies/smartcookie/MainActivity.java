022b2bdade64deb8782ef3fc79d5797c09866a44
Papavasileiou <antoniopi@csd.auth.gr>
2021-05-06 03:04:20 +0300

    Continue to recipe button works and takes the activity to acitvity with steps Κατέβασα το adobe XD για να κανω το design της εφαρμογης εκεί και μετα να το μεταφέρω Θα δουλέψω σήμερα το design του welcome page/ main menu και θα το κανω commit αυριο

--- app/src/main/java/com/authandroid_smartcookies/smartcookie/MainActivity.java
+++ app/src/main/java/com/authandroid_smartcookies/smartcookie/MainActivity.java
@@ -2,16 +2,27 @@ package com.authandroid_smartcookies.smartcookie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ContinueButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ContinueButton = findViewById(R.id.Continuetorecipebutton);
 
+        ContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this , Steps.class);
+                startActivity(intent);
+            }
+        });
     }
 }
