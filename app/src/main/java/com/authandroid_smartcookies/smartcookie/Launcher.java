c4879f10d2b7aa16dd7be1213b44c052fbd647e4
salapialk <salapialk@csd.auth.gr>
2021-05-15 23:29:49 +0300

    Launcher with animation + fonts

--- app/src/main/java/com/authandroid_smartcookies/smartcookie/Launcher.java
+++ app/src/main/java/com/authandroid_smartcookies/smartcookie/Launcher.java
@@ -1,29 +1,45 @@
 package com.authandroid_smartcookies.smartcookie;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Launcher extends AppCompatActivity {
+
+    Animation launcher_shaker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Handler handler ;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            );
         }
-        handler = new Handler();
-        handler.postDelayed(() -> {
-            Intent intent = new Intent(Launcher.this,HomeActivity.class);
-            startActivity(intent);
-            finish();
+
+        launcher_shaker = AnimationUtils.loadAnimation(this,R.anim.shaker);
+
+
+        final ImageView image1 = findViewById(R.id.image1);
+        image1.setAnimation(launcher_shaker);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Launcher.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }, 3000);
-
-
-
-
     }
 }
